bbb1212c05baf99e61750100cbb704623d8930aa
Pranil Napit <pranilnapit78@gmail.com>
2025-12-10 17:02:04 -0500

    User Registration
    User login and registration

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 import pymysql
 import yfinance as yf
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'
 
 # Database configurations
 def get_db_connection():
@@ -56,7 +58,7 @@ class Stock:
 
 
 @app.route('/')
-def portfolio():
+def home():
 # First version
 # ------------------------------------------------------
 #     conn = get_db_connection()
@@ -93,8 +95,101 @@ def portfolio():
     
 # ------------------------------------------------------
 # Using the class method to get all stocks
+
+    if "user_id" in session:
+        rows = Stock.get_all_stocks()
+        return render_template('stocks.html', rows=rows)
+    
+    return redirect(url_for('login'))
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        insert_query = "INSERT INTO users (username, password) VALUES (%s, %s)"
+        cursor.execute(insert_query, (username, password))
+        conn.commit()
+        
+        flash('Registration successful! Please log in.', 'success')
+        
+        return redirect(url_for('login'))
+        
+    return render_template('register.html')
+        
+        
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cursor.fetchone()
+        
+        if user and check_password_hash(user['password'], password):
+            session['user_id'] = user['id']
+            flash('Login successful!', 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Invalid username or password', 'danger')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('login'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+    
     rows = Stock.get_all_stocks()
-    return render_template('stocks.html', rows=rows)
+    conn = get_db_connection()
+    
+    investments = conn.execute("SELECT * FROM investments where user_id = %s", (session['user_id'],)).fetchall()
+    
+    total_value, total_cost = 0
+    
+    updated_investments = []
+    for inv in investments:
+        try:
+            #fetching live price using yfinance
+            ticker = yf.Ticker(inv["symbol"])
+            live_price = ticker.history(period='1d')['Close'].iloc[-1]
+        except Exception:
+            live_price = 0 # Default to 0 if fetching fails
+            
+        current_value = live_price * inv["quantity"]
+        cost = inv["quantity"] * inv["buy_price"]
+        
+        total_value += current_value
+        total_cost += cost
+        
+        updated_investments.append({
+            "id": inv["id"],
+            "symbol": inv["symbol"],
+            "category": inv["category"],
+            "quantity": inv["quantity"],
+            "buy_price": inv["buy_price"],
+            "current_price": round(live_price, 2),
+            "value": round(current_value, 2),
+            "profit_loss": round(current_value - cost, 2)
+        })
+        
+    profit_loss = total_value - total_cost
+        
+    return render_template('dashboard.html', rows=rows, investments=updated_investments, total_value=round(total_value,2), total_cost=round(total_cost,2), profit_loss=round(profit_loss,2))
+
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_stock():
